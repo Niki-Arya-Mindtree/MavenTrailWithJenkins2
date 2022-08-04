a1c2a1dba5659f8cd5a3b27a97e4972515269157
Niki-Arya-Mindtree <M1092216@mindtree.com>
2022-08-04 12:28:00 +0530

    FirstCommit

---
+++ MavenJenkinsGitTrail1-SetUp/src/main/java/MavenJenkinsGitTrail1/MavenJenkinsGitTrail1_SetUp/App.java
@@ -0,0 +1,9 @@
+package MavenJenkinsGitTrail1.MavenJenkinsGitTrail1_SetUp;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ MavenJenkinsGitTrail1-SetUp/src/test/java/MavenJenkinsGitTrail1/MavenJenkinsGitTrail1_SetUp/AppTest.java
@@ -0,0 +1,20 @@
+package MavenJenkinsGitTrail1.MavenJenkinsGitTrail1_SetUp;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
